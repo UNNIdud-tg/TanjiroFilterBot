55a43b97323cd063d77dbecff20dc1ba3524103e
UNNIdud-tg <UNNIduo@hotmail.com>
2025-01-26 17:53:26 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -100,11 +100,23 @@ This module only works for my admins
 • /unban  - <code>to unban a user.</code>
 • /channel - <code>to get list of total connected channels</code>
 • /broadcast - <code>to broadcast a message to all users</code>"""
-    STATUS_TXT = """★ 𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code>
-★ 𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂: <code>{}</code>
-★ 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code>
-★ 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱
-★ 𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱"""
+    STATUS_TXT = """<b><blockquote>📂Tᴏᴛᴀʟ Fɪʟᴇs :-<code>{}</code>
+
+
+Bᴏᴛ Usᴇʀs ᴀɴᴅ Cʜᴀᴛs Cᴏᴜɴᴛ
+
+👤Tᴏᴛᴀʟ Usᴇʀs: <code>{}</code>
+♻️Tᴏᴛᴀʟ Cʜᴀᴛs: <code>{}</code>
+
+1st Db
+📂Tᴏᴛᴀʟ Fɪʟᴇs: <code>{}</code>
+🗃️Usᴇᴅ Sᴛᴏʀᴀɢᴇ: <code>{} </code>
+🆓Fʀᴇᴇ Sᴛᴏʀᴀɢᴇ: <code>{} </code>
+
+2nd Db
+📂Tᴏᴛᴀʟ Fɪʟᴇs: <code>{}</code>
+🗃️Usᴇᴅ Sᴛᴏʀᴀɢᴇ: <code>{} </code>
+🆓Fʀᴇᴇ Sᴛᴏʀᴀɢᴇ: <code>{} </code></blockquote></b>"""
     LOG_TEXT_G = """#𝐍𝐞𝐰𝐆𝐫𝐨𝐮𝐩
     
 <b>᚛› 𝐆𝐫𝐨𝐮𝐩 ⪼ {}(<code>{}</code>)</b>
